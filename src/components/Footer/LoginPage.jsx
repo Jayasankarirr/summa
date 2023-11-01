d9ab65e3f0ebe471443a5a2adcf0c07283628e7c
sharmi-212 <118599027+sharmi-212@users.noreply.github.com>
2023-11-01 15:03:25 +0530

    Update LoginPage.jsx

--- src/components/Footer/LoginPage.jsx
+++ src/components/Footer/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import './LoginPage.scss';
+import './LoginPage.css';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
